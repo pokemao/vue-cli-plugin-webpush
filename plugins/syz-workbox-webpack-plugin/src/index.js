ee6b80413ae9fda3e9936a823278d9b67671d65b
yuzhe shao <william.yzshao@gmail.com>
2025-07-17 21:08:50 +0800

    feat: (apps-nopwa)init

--- plugins/syz-workbox-webpack-plugin/src/index.js
+++ plugins/syz-workbox-webpack-plugin/src/index.js
@@ -1,10 +1,12 @@
-const { sources } = require('webpack');
+const { sources } = require("webpack");
 
 class SyzModifySWPlugin {
   constructor(options = {}) {
+    console.log("shao options", options);
+
     this.options = {
-      swDest: 'service-worker.js', // 默认输出文件名
-      customCode: '', // 要追加的自定义代码
+      swDest: "service-worker.js", // 默认输出文件名
+      customCode: "", // 要追加的自定义代码
       ...options,
     };
   }
@@ -12,33 +14,36 @@ class SyzModifySWPlugin {
   apply(compiler) {
     const { Compilation } = compiler.webpack;
 
-    compiler.hooks.compilation.tap('SyzModifySWPlugin', (compilation) => {
-      compilation.hooks.processAssets.tap({
-        name: 'SyzModifySWPlugin',
-        stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE, // 在 Workbox 生成资产后运行
-      }, (assets) => {
-        console.log('shao', assets);
-        
-        // // 获取 service-worker.js 文件
-        // const swAsset = assets[this.options.swDest];
-        // if (!swAsset) {
-        //   compilation.warnings.push(
-        //     new Error(`Service worker file ${this.options.swDest} not found in assets.`)
-        //   );
-        //   return;
-        // }
+    compiler.hooks.compilation.tap("SyzModifySWPlugin", (compilation) => {
+      compilation.hooks.processAssets.tap(
+        {
+          name: "SyzModifySWPlugin",
+          stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE, // 在 Workbox 生成资产后运行
+        },
+        (assets) => {
+          console.log("shao", assets);
 
-        // // 获取文件内容
-        // let source = swAsset.source().toString();
+          // // 获取 service-worker.js 文件
+          // const swAsset = assets[this.options.swDest];
+          // if (!swAsset) {
+          //   compilation.warnings.push(
+          //     new Error(`Service worker file ${this.options.swDest} not found in assets.`)
+          //   );
+          //   return;
+          // }
 
-        // // 追加自定义代码
-        // if (this.options.customCode) {
-        //   source += `\n\n// Custom code injected by SyzModifySWPlugin\n${this.options.customCode}`;
-        // }
+          // // 获取文件内容
+          // let source = swAsset.source().toString();
 
-        // // 更新资产
-        // assets[this.options.swDest] = new sources.RawSource(source);
-      });
+          // // 追加自定义代码
+          // if (this.options.customCode) {
+          //   source += `\n\n// Custom code injected by SyzModifySWPlugin\n${this.options.customCode}`;
+          // }
+
+          // // 更新资产
+          // assets[this.options.swDest] = new sources.RawSource(source);
+        }
+      );
     });
   }
 }
--- plugins/vue-cli-plugin-webpush/index.js
+++ plugins/vue-cli-plugin-webpush/index.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const { chalk, warn } = require('@vue/cli-shared-utils')
+const fs = require("fs");
+const { chalk, warn } = require("@vue/cli-shared-utils");
 
 module.exports = (api, options) => {
-  const userOptions = options.pwa || {}
+  const userOptions = options.pwa || {};
   // const pwaMode = userOptions.workboxPluginMode ?? 'GenerateSW'
   // const workboxOptions = userOptions.workboxOptions
 
@@ -22,46 +22,55 @@ module.exports = (api, options) => {
   //   return 1
   // }
 
-  const manifestPath = api.resolve('public/manifest.json')
+  const manifestPath = api.resolve("public/manifest.json");
   if (fs.existsSync(manifestPath)) {
     if (!userOptions.manifestOptions) {
-      userOptions.manifestOptions = require(manifestPath)
+      userOptions.manifestOptions = require(manifestPath);
     } else {
       warn(
-        `The ${chalk.red('public/manifest.json')} file will be ignored in favor of ${chalk.cyan('pwa.manifestOptions')}`
-      )
+        `The ${chalk.red(
+          "public/manifest.json"
+        )} file will be ignored in favor of ${chalk.cyan(
+          "pwa.manifestOptions"
+        )}`
+      );
     }
   }
 
-  api.chainWebpack(webpackConfig => {
-    const target = process.env.VUE_CLI_BUILD_TARGET
-    if (target && target !== 'app') {
-      return
+  api.chainWebpack((webpackConfig) => {
+    const target = process.env.VUE_CLI_BUILD_TARGET;
+    if (target && target !== "app") {
+      return;
     }
 
-    const name = api.service.pkg.name
+    const name = api.service.pkg.name;
 
     // the pwa plugin hooks on to html-webpack-plugin
     // and injects icons, manifest links & other PWA related tags into <head>
     webpackConfig
-      .plugin('pwa')
-        .use(require('./lib/HtmlPwaPlugin'), [Object.assign({
-          name
-        }, userOptions)])
-        .after('html')
+      .plugin("pwa")
+      .use(require("./lib/HtmlPwaPlugin"), [
+        Object.assign(
+          {
+            name,
+          },
+          userOptions
+        ),
+      ])
+      .after("html");
 
     // generate /service-worker.js in production mode
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === "production") {
       // Default to GenerateSW mode, though InjectManifest also might be used.
-      const workboxPluginMode = userOptions.workboxPluginMode || 'GenerateSW'
-      const workboxWebpackModule = require('workbox-webpack-plugin')
-      const syzWorkboxWebpackModule = require('')
+      const workboxPluginMode = userOptions.workboxPluginMode || "GenerateSW";
+      const workboxWebpackModule = require("workbox-webpack-plugin");
+      const syzWorkboxWebpackModule = require("syz-workbox-webpack-plugin");
 
       if (!(workboxPluginMode in workboxWebpackModule)) {
         throw new Error(
           `${workboxPluginMode} is not a supported Workbox webpack plugin mode. ` +
-          `Valid modes are: ${Object.keys(workboxWebpackModule).join(', ')}`
-        )
+            `Valid modes are: ${Object.keys(workboxWebpackModule).join(", ")}`
+        );
       }
 
       const defaultOptions = {
@@ -69,25 +78,31 @@ module.exports = (api, options) => {
           /\.map$/,
           /img\/icons\//,
           /favicon\.ico$/,
-          /^manifest.*\.js?$/
-        ]
-      }
+          /^manifest.*\.js?$/,
+        ],
+      };
 
-      const defaultGenerateSWOptions = workboxPluginMode === 'GenerateSW'
-        ? { cacheId: name }
-        : {}
+      const defaultGenerateSWOptions =
+        workboxPluginMode === "GenerateSW" ? { cacheId: name } : {};
 
-      const workBoxConfig = Object.assign(defaultOptions, defaultGenerateSWOptions, userOptions.workboxOptions)
+      const workBoxConfig = Object.assign(
+        defaultOptions,
+        defaultGenerateSWOptions,
+        userOptions.workboxOptions
+      );
 
       webpackConfig
-        .plugin('workbox')
-        .use(workboxWebpackModule[workboxPluginMode], [workBoxConfig])
+        .plugin("workbox")
+        .use(workboxWebpackModule[workboxPluginMode], [workBoxConfig]);
+      webpackConfig
+        .plugin("syzworkboxwebpackplugin")
+        .use(syzWorkboxWebpackModule, { swDest: "service-worker-syz.js" });
     }
-  })
+  });
 
   // install dev server middleware for resetting service worker during dev
-  const createNoopServiceWorkerMiddleware = require('./lib/noopServiceWorkerMiddleware')
-  api.configureDevServer(app => {
-    app.use(createNoopServiceWorkerMiddleware())
-  })
-}
+  const createNoopServiceWorkerMiddleware = require("./lib/noopServiceWorkerMiddleware");
+  api.configureDevServer((app) => {
+    app.use(createNoopServiceWorkerMiddleware());
+  });
+};
